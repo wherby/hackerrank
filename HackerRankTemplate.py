e7487a61ee40fb7524b424605895061f818f1cd6
wherby <187225577@qq.com>
2016-09-02 21:56:10 +0800

    add existed code

---
+++ HackerRankTemplate.py
@@ -0,0 +1,19 @@
+filename = "input00.txt"
+f=open(filename,'r')
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+import sys
+
+if  "f" in locals():
+    inputA=f
+else:
+    inputA=sys.stdin
+
+
+ins=[]
+for line in inputA:
+    ins.append(line)
+
+
+print ins
