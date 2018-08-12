e4610a7d6c995b46fd65f6d03779d049ecc5c8e8
wherby <187225577@qq.com>
2018-08-12 13:21:32 +0800

    add safemap in python

--- Test/ZipLike/zipLike.py
+++ Test/ZipLike/zipLike.py
@@ -1,5 +1,8 @@
+
 a=[(1,2,3),(4,5,6),(7,8,9)]
 print(zip(*a))
+print(map(None,*a))
+#print(map(max,*a))
 
 f1 = lambda(a): [tuple(a[j][i] for j in range(len(a))) for i in range(len(a[0]))]
 print f1(a)
@@ -10,3 +13,15 @@ print zip(*b)
 
 f2 = lambda(a): [tuple(a[j][i] for j in range(len(a))) for i in range(min(map(len,a)))]
 print f2(b)
+arg = a
+print(zip(*arg))
+
+
+
+
+
+
+
+
+
+
