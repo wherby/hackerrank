f9a2fea833dd9f7e8c3c933573979ff69feea82d
wherby <187225577@qq.com>
2018-08-18 01:27:06 +0800

    add dotnet Thermomeeter and safeMap

--- Test/ZipLike/zipLike.py
+++ Test/ZipLike/zipLike.py
@@ -10,6 +10,7 @@ print f1(a)
 b = [(1,2,3),(4,5,6),(7,8,9),(10,11)]
 print zip(*b)
 #print f1(b)
+print map(None,*b)
 
 f2 = lambda(a): [tuple(a[j][i] for j in range(len(a))) for i in range(min(map(len,a)))]
 print f2(b)
