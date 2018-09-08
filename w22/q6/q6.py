8a068f23a52920cf5179ba0b64971889a56ea7da
wherby <187225577@qq.com>
2018-09-08 20:22:18 +0800

    add persistent array

--- w22/q6/q6.py
+++ w22/q6/q6.py
@@ -10,9 +10,9 @@ class Node:
 class PersistentArray:
     def __init__(self):
         self.L=0
-        self.R=10**6
-        self.M = 20* 10**6
-        self.pool = [Node(-1) for i in range(self.M)]
+        self.R=2*10**5
+        self.M = 20* 2* 10**5
+        self.pool = [Node(-1) for _ in range(self.M)]
 
 print(30*10**4)
 a = PersistentArray()
