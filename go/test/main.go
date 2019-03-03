8fc56f81bdd9d192678be32feb10257d6d3c081a
wherby <187225577@qq.com>
2019-03-03 17:22:28 +0800

    sync

--- go/test/main.go
+++ go/test/main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+
+
+func reverse(x int) int {
+
+
+	var re = 0
+	for ; x !=0; {
+		e := x%10
+		x = x/10
+		re = re *10 +e
+	}
+	if int(int32(re)) != re{
+		return 0
+	}
+	return re
+}
+
 func main() {
-	fmt.Println("Hello, 世界")
+	fmt.Println(reverse(-2147483648))
 }
